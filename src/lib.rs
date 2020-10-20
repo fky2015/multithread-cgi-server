5a2c474049d2e3aea9f3cbfdd78a9843f0866faf
FKYnJYQ <loveress01@gmail.com>
2020-10-20 23:40:19 +0800

    feat(thread): impl thread pool management and gracefully exit
    Signed-off-by: FKYnJYQ <loveress01@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
-use std::error::Error;
+use std::sync::{mpsc, Arc, Mutex};
+use std::thread;
 
-pub struct ThreadPool;
+#[derive(Debug)]
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Message>,
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
     /// 创建线程池。
@@ -14,14 +21,109 @@ impl ThreadPool {
         if size == 0 {
             Err("size must larger than 0.".to_string())
         } else {
-            Ok(ThreadPool)
+            let (sender, receiver) = mpsc::channel();
+
+            let mut workers = Vec::with_capacity(size);
+
+            let receiver = Arc::new(Mutex::new(receiver));
+
+            for id in 0..size {
+                workers.push(Worker::new(id, Arc::clone(&receiver)));
+            }
+            Ok(ThreadPool { workers, sender })
         }
     }
 
     pub fn execute<F>(&self, f: F)
-    where
-        F: FnOnce() + Send + 'static,
+        where
+            F: FnOnce() + Send + 'static,
     {
-        f()
+        let job = Box::new(f);
+
+        self.sender.send(Message::NewJob(job)).unwrap();
     }
+
+    pub fn manual_drop(&mut self) {
+        println!("Sending terminate message to all workers.");
+
+        for _ in &mut self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        println!("Shutting down all workers.");
+
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
+// impl Drop for ThreadPool {
+//     // fn drop(&mut self) {
+//     //     for worker in &mut self.workers {
+//     //         println!("Shutting down worker {}", worker.id);
+//     //
+//     //         if let Some(thread) = worker.thread.take() {
+//     //             thread.join().unwrap();
+//     //         }
+//     //     }
+//     // }
+//     fn drop(&mut self) {
+//         println!("Sending terminate message to all workers.");
+//
+//         for _ in &mut self.workers {
+//             self.sender.send(Message::Terminate).unwrap();
+//         }
+//
+//         println!("Shutting down all workers.");
+//
+//         for worker in &mut self.workers {
+//             println!("Shutting down worker {}", worker.id);
+//
+//             if let Some(thread) = worker.thread.take() {
+//                 thread.join().unwrap();
+//             }
+//         }
+//     }
+// }
+
+#[derive(Debug)]
+struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let message = receiver.lock().unwrap().recv().unwrap();
+
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker {} got a job; executing.", id);
+
+                    job();
+                }
+                Message::Terminate => {
+                    println!("Worker {} was told to terminate.", id);
+
+                    break;
+                }
+            };
+        });
+
+        Worker {
+            id,
+            thread: Some(thread),
+        }
+    }
+}
+
+enum Message {
+    NewJob(Job),
+    Terminate,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use dotenv;
+use multithread_cgi_server::ThreadPool;
 use std::env;
 use std::error::Error;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
-use multithread_cgi_server::ThreadPool;
+use std::sync::{Arc, Mutex};
+use ctrlc;
+use std::process::exit;
 
 fn main() -> Result<(), Box<dyn Error>> {
     dotenv::dotenv().ok();
@@ -11,16 +14,27 @@ fn main() -> Result<(), Box<dyn Error>> {
     let host = env::var("HOST").unwrap_or_else(|_| "127.0.0.1".into());
     let port = env::var("HOST").unwrap_or_else(|_| "8000".into());
     let listener = TcpListener::bind(format!("{}:{}", host, port)).unwrap();
-    let pool = ThreadPool::new(4)?;
+    let pool = Arc::new(Mutex::new(ThreadPool::new(4)?));
+
+    let pool_handler = pool.clone();
+
+    // (Almost) Gracefully exit.
+    ctrlc::set_handler(move || {
+        let mut pool = pool_handler.lock().unwrap();
+        pool.manual_drop();
+        exit(0);
+    }).unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
+        let pool = pool.lock().unwrap();
         pool.execute(|| {
             handle_connection(stream);
         });
     }
 
+    println!("Shutting down.");
     Ok(())
 }
 
@@ -43,5 +57,5 @@ fn handle_connection(mut stream: TcpStream) {
     stream.flush().unwrap();
 
     // TODO: logging
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
