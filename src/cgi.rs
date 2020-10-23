75b2fbb0d96f4c9844eb329e610a3edb4c078e65
ErvinXie <465265950@qq.com>
2020-10-23 10:44:30 +0800

    cgi router ok

--- src/cgi.rs
+++ src/cgi.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use std::process::Command;
+use std::env;
 
 #[cfg(test)]
 mod cgi_tests {
@@ -8,11 +9,21 @@ mod cgi_tests {
 
 
     #[test]
-    fn command_test() {
-        let path = "ls".to_string();
+    fn calculator_test1() {
+        let path = "./cgi-part/cgi-bin/calculator.py".to_string();
+        let result = cgi_command(&path, &"value2=234&value1=123".to_string());
+        assert_eq!(("Content-type:text/html\n\n<html>\n<head>\n<meta charset=\"utf-8\">\n<title>两数之和与之积</title>\n</head>\n<body>\n<h2>两数之和: 357</h2>\n<h2>两数之积: 28782</h2>\n</body>\n</html>\n".to_string()
+                   ,true),
+                   result);
+    }
 
-        let result = cgi_command(&path, &"123".to_string());
-        assert_eq!("Cargo.lock\nCargo.toml\nsrc\ntarget\n".to_string(), result);
+    #[test]
+    fn calculator_test2() {
+        let path = "./cgi-part/cgi-bin/calculator.py".to_string();
+        let result = cgi_command(&path, &"xxx".to_string());
+        assert_eq!(("".to_string()
+                    ,false),
+                   result);
     }
 
     #[test]
@@ -21,17 +32,41 @@ mod cgi_tests {
         key_values.insert("value1".to_string(), "123".to_string());
         key_values.insert("value2".to_string(), "234".to_string());
         let re = kv_to_env(key_values);
+        println!("{}", re);
         assert!(("value2=234&value1=123" == re ||
-            "value2=234&value1=123" == re));
+            "value1=123&value2=234" == re));
     }
+
+    #[test]
+    fn cgi_test1(){
+        let mut key_values = HashMap::new();
+        key_values.insert("value1".to_string(), "123".to_string());
+        key_values.insert("value2".to_string(), "234".to_string());
+        let path = "./cgi-part/cgi-bin/calculator.py".to_string();
+        assert_eq!(("Content-type:text/html\n\n<html>\n<head>\n<meta charset=\"utf-8\">\n<title>两数之和与之积</title>\n</head>\n<body>\n<h2>两数之和: 357</h2>\n<h2>两数之积: 28782</h2>\n</body>\n</html>\n".to_string(),
+                    true) ,cgi_caller(&path,key_values));
+    }
+
+    #[test]
+    fn cgi_test2(){
+        let mut key_values = HashMap::new();
+        key_values.insert("vae1".to_string(), "123".to_string());
+        key_values.insert("value2".to_string(), "234".to_string());
+        let path = "./cgi-part/cgi-bin/calculator.py".to_string();
+        assert_eq!(("".to_string(),false) ,cgi_caller(&path,key_values));
+    }
+
 }
 
 fn cgi_command(
     path: &String,
-    env: &String,
-) -> String {
-    let output = Command::new(path).output();
-    String::from_utf8(output.unwrap().stdout).unwrap()
+    env_string: &String,
+) -> (String, bool) {
+    let output = Command::new(path)
+        .env("QUERY_STRING", env_string)
+        .output()
+        .expect("Failed to execute process");
+    (String::from_utf8(output.stdout).unwrap(), output.status.success())
 }
 
 fn kv_to_env(
@@ -50,14 +85,8 @@ fn kv_to_env(
 fn cgi_caller(
     path: &String,
     key_values: HashMap<String, String>,
-) -> String {
-    let mut env: String = "".to_string();
-    for (key, value) in &key_values {
-        env.push('&');
-        env.push_str(key.as_str());
-        env.push('=');
-        env.push_str(value.as_str());
-    }
-    env[1..].parse().unwrap()
+) -> (String,bool) {
+    let env_string = kv_to_env(key_values);
+    cgi_command(&path,&env_string)
 }
 
