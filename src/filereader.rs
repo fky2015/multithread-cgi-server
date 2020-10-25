b45b32c19399e47d2fad9dcaa4ed4015a10a170b
yuanruize@sina.com <yuanruize@sina.com>
2020-10-25 23:23:34 +0800

    add content-type

--- src/filereader.rs
+++ src/filereader.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::path::PathBuf;
 
-pub fn readfile(filepath: String) -> String {
+pub fn readfile(filepath: String) -> (String, String) {
     let mut content = String::new();
     let mut pathbuf = PathBuf::from("");
     pathbuf.push("static");
@@ -13,7 +13,23 @@ pub fn readfile(filepath: String) -> String {
     pathbuf.push(path);
     let mut pathstr = pathbuf.as_path().display().to_string();
     // pathstr = (&pathstr[1..]).to_string();
-    println!("{}", pathstr);
+    // println!("{}", pathstr);
+
+    let tokens:Vec<&str> = filepath.split(".").collect();
+    let extender = tokens[tokens.len() - 1];
+    let content_type = match extender {
+        "html" => "text/html",
+        "xml" => "text/xml",
+        "css" => "text/css",
+        "gif" => "image/gif",
+        "jpg" => "image/jpg",
+        "jpeg" => "image/jpeg",
+        "png" => "image/png",
+        "ico" => "application/x-ico",
+        _ => "text/html"
+    }.to_string();
+    // println!("content-type : {}", content_type);
+
     let mut file = match File::open(pathstr) {
         Ok(mut file) => {
             println!("ok");
@@ -25,23 +41,23 @@ pub fn readfile(filepath: String) -> String {
             <body><h3>Sorry, the file you were looking for was not found - 404</h3></body></html>".to_string();
         }
     };
-    content
+    (content, content_type)
 }
 
     #[test]
     fn file_test1() {
         let res = readfile("/index.html".to_string());
-        println!("{}", res);
+        println!("{}\n{}\n", res.0, res.1);
     }
 
     #[test]
     fn file_tes21() {
         let res = readfile("index.html".to_string());
-        println!("{}", res);
+        println!("{}\n{}\n", res.0, res.1);
     }
 
     #[test]
     fn file_test3() {
         let res = readfile("index1.html".to_string());
-        println!("{}", res);
+        println!("{}\n{}\n", res.0, res.1);
     }
