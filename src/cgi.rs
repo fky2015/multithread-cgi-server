31bdbe1d4d50918fe8c76c73284f32b241ee5b86
ErvinXie <465265950@qq.com>
2020-10-26 00:36:27 +0800

    cgi get ok

--- src/cgi.rs
+++ src/cgi.rs
@@ -1,94 +1,133 @@
-use std::collections::HashMap;
-use std::process::Command;
 use std::env;
+use std::io::{self, Write};
+use std::process::{Command, Stdio};
+use std::path::PathBuf;
+
+use crate::cgi::CgiCallError::{RuntimeError, FileNotExists};
+
+const CGI_ROOT_PATH: &str = "./cgi-part";
+
+#[derive(Debug)]
+#[derive(PartialEq)]
+pub enum CgiCallError {
+    FileNotExists,
+    RuntimeError,
+}
 
 // call this function with path and key-values
 // return HTML and ExitStatus of cgi-bin
 // See tests for example
-pub fn cgi_caller(
-    path: &String,
-    key_values: HashMap<String, String>,
-) -> (String,bool) {
-    let env_string = kv_to_env(key_values);
-    cgi_command(&path,&env_string)
-}
-fn cgi_command(
-    path: &String,
-    env_string: &String,
-) -> (String, bool) {
-    let output = Command::new(path)
-        .env("QUERY_STRING", env_string)
-        .output()
-        .expect("Failed to execute process");
-    (String::from_utf8(output.stdout).unwrap(), output.status.success())
+pub fn cgi_caller_get(
+    cgi_path: &str,
+    query_string: &str,
+) -> Result<String, CgiCallError> {
+    let real_path: PathBuf = [CGI_ROOT_PATH, cgi_path].iter().collect();
+    if !real_path.exists() {
+        return Err(FileNotExists);
+    }
+
+    let output = Command::new(real_path.to_str().unwrap())
+        .env("METHOD", "GET")
+        .env("QUERY_STRING", query_string)
+        .output().unwrap();
+
+    match output.status.success() {
+        true => {
+            let re = String::from_utf8(output.stdout).unwrap();
+            Ok(re)
+        }
+        false => Err(RuntimeError),
+    }
 }
 
-fn kv_to_env(
-    key_values: HashMap<String, String>,
-) -> String {
-    let mut re: String = "".to_string();
-    for (key, value) in &key_values {
-        re.push('&');
-        re.push_str(key.as_str());
-        re.push('=');
-        re.push_str(value.as_str());
+pub fn cgi_caller_post(
+    cgi_path: &str,
+    content_lenght: &str,
+    content_type: &str,
+    body_string: &str,
+) -> Result<String, CgiCallError> {
+    let real_path: PathBuf = [CGI_ROOT_PATH, cgi_path].iter().collect();
+    if !real_path.exists() {
+        return Err(FileNotExists);
     }
-    re[1..].parse().unwrap()
+
+    let mut child = Command::new(real_path.to_str().unwrap())
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .env("METHOD", "POST")
+        .env("CONTENT_LENGTH", content_lenght)
+        .env("CONTENT_TYPE", content_type)
+        .spawn().unwrap();
+
+    {
+        let child_stdin = child.stdin.as_mut().unwrap();
+        child_stdin.write_all(body_string.as_ref()).unwrap();
+    }
+
+    let output = child.wait_with_output().unwrap();
+    let re = String::from_utf8(output.stdout).unwrap();
+    println!("{}",String::from_utf8(output.stderr).unwrap());
+    println!("{}",re);
+
+
+    match output.status.success() {
+        true => {
+
+            Ok(re)
+        }
+        false => Err(RuntimeError),
+    }
 }
+
+
+fn cgi_file_exists(cgi_path: &str) -> bool {
+    use std::path::Path;
+    Path::new(cgi_path).exists()
+}
+
 
 #[cfg(test)]
 mod cgi_tests {
-    use crate::cgi::{cgi_caller, cgi_command, kv_to_env};
-    use std::collections::HashMap;
+    use crate::cgi::{cgi_caller_get, cgi_caller_post, cgi_file_exists, CgiCallError};
 
 
     #[test]
-    fn calculator_test1() {
-        let path = "./cgi-part/cgi-bin/calculator.py".to_string();
-        let result = cgi_command(&path, &"value2=234&value1=123".to_string());
-        assert_eq!(("Content-type:text/html\n\n<html>\n<head>\n<meta charset=\"utf-8\">\n<title>两数之和与之积</title>\n</head>\n<body>\n<h2>两数之和: 357</h2>\n<h2>两数之积: 28782</h2>\n</body>\n</html>\n".to_string()
-                    ,true),
+    fn get_test1() {
+        let query_string = "value1=123&value2=234";
+        let path = "cgi-bin/calculator.py";
+        let result = cgi_caller_get(path, query_string).unwrap_or("".to_string());
+        assert_eq!("Content-type:text/html\n\n<html>\n<head>\n<meta charset=\"utf-8\">\n<title>两数之和与之积</title>\n</head>\n<body>\n<h2>两数之和: 357</h2>\n<h2>两数之积: 28782</h2>\n</body>\n</html>\n".to_string(),
                    result);
     }
 
     #[test]
-    fn calculator_test2() {
-        let path = "./cgi-part/cgi-bin/calculator.py".to_string();
-        let result = cgi_command(&path, &"xxx".to_string());
-        assert_eq!(("".to_string()
-                    ,false),
+    fn get_test2() {
+        let query_string = "value1=123&value2=234";
+        let path = "cgi-bin/not-exists";
+        let result = cgi_caller_get(path, query_string).err().unwrap();
+        assert_eq!(CgiCallError::FileNotExists, result);
+    }
+
+    #[test]
+    fn get_test3() {
+        let query_string = "value=123&val=234";
+        let path = "cgi-bin/calculator.py";
+        let result = cgi_caller_get(path, query_string).err().unwrap();
+        assert_eq!(CgiCallError::RuntimeError, result);
+    }
+
+    #[test]
+    fn post_test1() {
+        let query_string = "value=123&val=234";
+        let path = "cgi-bin/calculator.py";
+        let content_length = query_string.len().to_string();
+
+        let result = cgi_caller_post(path,
+                                     content_length.as_str(),
+                                     "application/x-www-form-urlencoded",
+                                     query_string).unwrap();
+        assert_eq!("Content-type:text/html\n\n<html>\n<head>\n<meta charset=\"utf-8\">\n<title>两数之和与之积</title>\n</head>\n<body>\n<h2>两数之和: 357</h2>\n<h2>两数之积: 28782</h2>\n</body>\n</html>\n".to_string(),
                    result);
     }
-
-    #[test]
-    fn kv_test() {
-        let mut key_values = HashMap::new();
-        key_values.insert("value1".to_string(), "123".to_string());
-        key_values.insert("value2".to_string(), "234".to_string());
-        let re = kv_to_env(key_values);
-        println!("{}", re);
-        assert!(("value2=234&value1=123" == re ||
-            "value1=123&value2=234" == re));
-    }
-
-    #[test]
-    fn cgi_test1(){
-        let mut key_values = HashMap::new();
-        key_values.insert("value1".to_string(), "123".to_string());
-        key_values.insert("value2".to_string(), "234".to_string());
-        let path = "./cgi-part/cgi-bin/calculator.py".to_string();
-        assert_eq!(("Content-type:text/html\n\n<html>\n<head>\n<meta charset=\"utf-8\">\n<title>两数之和与之积</title>\n</head>\n<body>\n<h2>两数之和: 357</h2>\n<h2>两数之积: 28782</h2>\n</body>\n</html>\n".to_string(),
-                    true) ,cgi_caller(&path,key_values));
-    }
-
-    #[test]
-    fn cgi_test2(){
-        let mut key_values = HashMap::new();
-        key_values.insert("vae1".to_string(), "123".to_string());
-        key_values.insert("value2".to_string(), "234".to_string());
-        let path = "./cgi-part/cgi-bin/calculator.py".to_string();
-        assert_eq!(("".to_string(),false) ,cgi_caller(&path,key_values));
-    }
-
 }
 
